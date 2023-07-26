efceb474dae351e5e0449d90cb070e7e99095792
Aman Mathur <amanmathur27.am@gmail.com>
2023-07-26 18:13:30 +0530

    Updated gcd codes

--- gcd.cpp
+++ gcd.cpp
@@ -1,6 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
 
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    else if (b == 0)
+        return a;
+    else
+    {
+        while (a != b)
+        {
+            if (a > b)
+            {
+                a = a - b;
+            }
+            else
+            {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+}
+int main()
+{
+    int a, b, ans;
+    cout << "Enter the two numbers\n";
+    cin >> a >> b;
+    ans = gcd(a, b);
+    cout << "The GCD of " << a << " and " << b << " is " << ans;
     return 0;
 }
